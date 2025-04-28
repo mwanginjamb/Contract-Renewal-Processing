a747401b8ae1a5567d2180f90db606d4124cb857
mwanginjamb <fnjambi@outlook.com>
2025-04-28 20:35:21 +0300

    Refactor: add width specifications to notices email templates

--- common/mail/workflowNotification-html.php
+++ common/mail/workflowNotification-html.php
@@ -37,7 +37,7 @@ $verifyLink = Yii::$app->urlManager->createAbsoluteUrl(['contracts/approvals']);
     <?= Html::a('View Signing Entries', $verifyLink, ['style' => 'text-decoration:none;background-color: #007bff;color: #ffffff;border: none;padding: 10px 20px;font-size: 14px;font-weight: bold;cursor: pointer;border-radius: 5px;transition: background-color 0.3s;']) ?>
 </div>
 <footer
-    style="margin:1.5rem 0;text-align: center;font-size: 12px;border-top: 1px solid #dddddd;display:flex;justify-content:space-between;padding:0 10px;">
+    style="margin:1.5rem 0;text-align: center;font-size: 12px;border-top: 1px solid #dddddd;display:flex;justify-content:space-between;padding:0 10px;width: 100%; max-width: 600px;">
     <div style="color: #ffffff;padding: 10px;background-color:#5a5757;margin: 0;line-height: 1.5;">&copy;
         <?= date('Y') ?>
         <?= env('CUSTOMER', 'KEMRI ICT') ?>
